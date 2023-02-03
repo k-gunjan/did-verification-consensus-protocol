45c98a906e5e968d75d885ba3beb0da21c6e01b9
s5k0651 <suraj410401@gmail.com>
2023-02-04 03:06:07 +0530

    define benchmarks! pallet_babe

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -509,6 +509,7 @@ mod benches {
 	define_benchmarks!(
 		[frame_benchmarking, BaselineBench::<Runtime>]
 		[frame_system, SystemBench::<Runtime>]
+		[pallet_babe, Babe]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
 		[pallet_mmr, Mmr]
